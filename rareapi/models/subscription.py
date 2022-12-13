30c436def2b0f5c4a0ac9beea03e801378d83bcf
Justin Ferwerda <justin.ferwerda@gmail.com>
2022-12-12 21:04:37 -0600

    debugged

--- rareapi/models/subscription.py
+++ rareapi/models/subscription.py
@@ -5,7 +5,7 @@ from .user import User
 class Subscription(models.Model):
 
     follower_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    author_id = models.IntegerField(User, on_delete=models.CASCADE)
+    author_id = models.ForeignKey(User, on_delete=models.CASCADE)
     created_on = models.DateField()
     ended_on = models.DateField()
     
