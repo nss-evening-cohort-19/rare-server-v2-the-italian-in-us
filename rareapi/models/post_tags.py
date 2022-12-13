6adb5f221e89cf440d5d5241e0092947fdd3c6c8
nishaya raj <nishayaraj@gmail.com>
2022-12-12 21:33:24 -0600

    changes

--- rareapi/models/post_tags.py
+++ rareapi/models/post_tags.py
@@ -1,8 +1,8 @@
 from django.db import models
 from .tags import Tags
-# from .post import Post
+from .post import Post
 
 class PostTags(models.Model):
   
   tag = models.ForeignKey(Tags, on_delete=models.CASCADE)
-  # post = models.ForeignKey(Post, on_delete=models.CASCADE)
+  post = models.ForeignKey(Post, on_delete=models.CASCADE)
