89cf459c1e6df0b072c07cfa33fe1ddb3ed69012
Sariah Campopiano <scampopiano925@gmail.com>
2022-12-12 21:17:15 -0600

    Added 'approver_one_id property to DemotionQueue

--- rareapi/models/demotion_queue.py
+++ rareapi/models/demotion_queue.py
@@ -5,3 +5,4 @@ class DemotionQueue(models.Model):
 
     action = models.CharField(max_length=100)
     admin_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    approver_one_id = models.IntegerField()
