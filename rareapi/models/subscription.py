f7aea4a46f70d43b6ad5e0506d1fe9f0248dfb13
Justin Ferwerda <justin.ferwerda@gmail.com>
2022-12-12 21:03:39 -0600

    changed to foreign keys for follower and author_id

--- rareapi/models/subscription.py
+++ rareapi/models/subscription.py
@@ -1,10 +1,11 @@
 from django.db import models
+from .user import User
 
 
 class Subscription(models.Model):
 
-    follower_id = models.IntegerField()
-    author_id = models.IntegerField()
+    follower_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    author_id = models.IntegerField(User, on_delete=models.CASCADE)
     created_on = models.DateField()
     ended_on = models.DateField()
     
