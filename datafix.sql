ee2837f83adb8c83c4ff52452fda597eaff86f97
nishaya raj <nishayaraj@gmail.com>
2022-12-15 20:31:16 -0600

    CRUD added and tested on Tags

--- datafix.sql
+++ datafix.sql
@@ -1,5 +1,2 @@
 DELETE from rareapi_user
   WHERE id = 2
-
-INSERT INTO rareapi_user (uid)
-  VALUES (eCErZ4H6EFPSmCoCThVoyAdNghJ2);
--- rare/urls.py
+++ rare/urls.py
@@ -15,7 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+# from rareapi.views import register_user, check_user
+from django.conf.urls import include
+from rest_framework import routers
+from rareapi.views import TagsView
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'tags', TagsView, 'tag')
 
 urlpatterns = [
+    # path('register', register_user),
+    # path('checkuser', check_user),
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
