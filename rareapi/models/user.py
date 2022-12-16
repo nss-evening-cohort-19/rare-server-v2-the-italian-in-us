48c3a42beb5ea1156cab876830b139860323f87e
Justin Ferwerda <justin.ferwerda@gmail.com>
2022-12-15 20:05:32 -0600

    got register view working

--- rareapi/models/user.py
+++ rareapi/models/user.py
@@ -3,11 +3,12 @@ from django.db import models
 
 class User(models.Model):
 
+    uid = models.CharField(max_length=50, default="X2B6BcDVXNbCHo1svFqlGmBpMWl1")
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     bio = models.CharField(max_length=280)
     profile_image_url = models.CharField(max_length=50)
     email = models.EmailField()
-    created_on = models.DateField()
+    created_on = models.DateField(auto_now_add=True)
     active = models.BooleanField()
     is_staff = models.BooleanField()
