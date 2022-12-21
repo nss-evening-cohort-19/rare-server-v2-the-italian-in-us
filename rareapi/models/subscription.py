941e87e8e24a86aa5cf1eef453a773826b8e8869
Justin Ferwerda <justin.ferwerda@gmail.com>
2022-12-20 18:47:18 -0600

    removed auto-Add-now=True from created_on att.

--- rareapi/models/subscription.py
+++ rareapi/models/subscription.py
@@ -6,4 +6,4 @@ class Subscription(models.Model):
 
     follower_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followers')
     author_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author')
-    created_on = models.DateField(auto_now_add=True)    
+    created_on = models.DateField()    
